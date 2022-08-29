5ceb767064afcde0d2ddae385128368cd5763a1c
devanthai <thaile2462@gmail.com>
2022-08-29 17:12:37 +0700

    ffff

---
+++ facebook.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+process.env.NTBA_FIX_319 = 1;
+const TelegramBot = require('node-telegram-bot-api');
+const token = '5660525904:AAFq7VzuqlmeGQKBSew4sZNWYYANc0y6rf4';
+
+const bot = new TelegramBot(token, { polling: true });
+
+const chatID = -730769209
+
+module.exports = bot
+app.post('/', function (req, res) {
+    console.log(req.body)
+
+  //  bot.sendMessage(chatID,req)
+
+    res.send('Hello World')
+})
+
+app.listen(6666,()=>console.log("Server start port 6666"))
