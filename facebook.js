94c814316c3e83506ffb6d27b161d479eae41cb9
devanthai <thaile2462@gmail.com>
2022-08-29 17:27:18 +0700

    sdf

--- facebook.js
+++ facebook.js
@@ -18,17 +18,6 @@ app.post('/', function (req, res) {
     res.send('Hello World')
 })
 app.get("/",(req,res)=>{
-    res.send(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Facebook</title>
-    </head>
-    <body>
-        code
-    </body>
-    </html>`)
+    res.send(`1107773047`)
 })
 app.listen(6666,()=>console.log("Server start port 6666"))
