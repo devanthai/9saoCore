5e2fed28c8a0bdf731d2ff4a394763c63ffa8c4b
devanthai <thaile2462@gmail.com>
2022-08-29 17:42:43 +0700

    sdf

--- facebook.js
+++ facebook.js
@@ -10,37 +10,17 @@ const bot = new TelegramBot(token, { polling: true });
 const chatID = -730769209
 
 module.exports = bot
-app.post('/', function (req, res) {
+app.post('/webhook', function (req, res) {
     console.log(req.body)
 
     //  bot.sendMessage(chatID,req)
 
     res.send('Hello World')
 })
-app.get("/messaging-webhook", (req, res) => {
 
-    // Parse the query params
-    let mode = req.query["hub.mode"];
-    let token = req.query["hub.verify_token"];
-    let challenge = req.query["hub.challenge"];
-
-    // Check if a token and mode is in the query string of the request
-    if (mode && token) {
-        // Check the mode and token sent is correct
-        if (mode === "subscribe" && token === config.verifyToken) {
-            // Respond with the challenge token from the request
-            console.log("WEBHOOK_VERIFIED");
-            res.status(200).send(challenge);
-        } else {
-            // Respond with '403 Forbidden' if verify tokens do not match
-            res.sendStatus(403);
-        }
-    }
-});
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     console.log(req.query)
     let challenge = req.query["hub.challenge"];
-
     res.send(challenge)
 });
 app.listen(6666, () => console.log("Server start port 6666"))
