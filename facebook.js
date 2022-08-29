1ea01cd232cd7f0d65023b2e9511997f7ff6d6bf
devanthai <thaile2462@gmail.com>
2022-08-29 17:34:21 +0700

    sdf

--- facebook.js
+++ facebook.js
@@ -37,24 +37,10 @@ app.get("/messaging-webhook", (req, res) => {
         }
     }
 });
-app.get("/", (req, res) => {
-    
-    // Parse the query params
-    let mode = req.query["hub.mode"];
-    let token = req.query["hub.verify_token"];
+app.get("*", (req, res) => {
+    console.log(req.query)
     let challenge = req.query["hub.challenge"];
 
-    // Check if a token and mode is in the query string of the request
-    if (mode && token) {
-        // Check the mode and token sent is correct
-        if (mode === "subscribe" && token === config.verifyToken) {
-            // Respond with the challenge token from the request
-            console.log("WEBHOOK_VERIFIED");
-            res.status(200).send(challenge);
-        } else {
-            // Respond with '403 Forbidden' if verify tokens do not match
-            res.sendStatus(403);
-        }
-    }
+    res.send(challenge)
 });
 app.listen(6666, () => console.log("Server start port 6666"))
