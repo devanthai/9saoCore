8207d4960ea8994199f55613403d57f5a536dc79
devanthai <thaile2462@gmail.com>
2022-10-03 11:42:39 +0700

    dfgdfg

--- public/js/newSocket.js
+++ public/js/newSocket.js
@@ -3,5 +3,5 @@ if (document.domain.includes("localhost")) {
     socketNew = io.connect("http://" + document.domain + ":9956");
 }
 else {
-    socketNew = io.connect("https://" + document.domain);
+    socketNew = io.connect("https://socket." + document.domain);
 }
