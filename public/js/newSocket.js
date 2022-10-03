0af5c5c6ad5f32126ddb226b3b54a7c261a333e1
devanthai <thaile2462@gmail.com>
2022-10-03 11:55:14 +0700

    sokcet update

--- public/js/newSocket.js
+++ public/js/newSocket.js
@@ -3,5 +3,5 @@ if (document.domain.includes("localhost")) {
     socketNew = io.connect("http://" + document.domain + ":9956");
 }
 else {
-    socketNew = io.connect("https://socket." + document.domain);
+    socketNew = io.connect("https://socket.9sao.me");
 }
