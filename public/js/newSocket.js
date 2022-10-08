a5f0f51bd29365aad36c9c7b76648fd1c63fffee
devanthai <thaile2462@gmail.com>
2022-10-08 18:16:44 +0700

    socket thai

--- public/js/newSocket.js
+++ public/js/newSocket.js
@@ -3,5 +3,8 @@ if (document.domain.includes("localhost")) {
     socketNew = io.connect("http://" + document.domain + ":9956");
 }
 else {
-    socketNew = io.connect("https://socket.9sao.me");
+    socketNew = io.connect("https://socket.9sao.me", { query: "username="+usernameUser });
 }
+socketNew.on("messageFromServer",(data)=>{
+    alert(data)
+})
