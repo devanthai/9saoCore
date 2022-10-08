49f79c03ec44fcefe4b34aa208461627e1fde28c
devanthai <thaile2462@gmail.com>
2022-10-08 18:52:05 +0700

    dfgdfg

--- SocketOnline.js
+++ SocketOnline.js
@@ -61,7 +61,7 @@ io.on("connection", (socket) => {
         }
     })
     //add player
-    SocketPlayer[socket.id] = { username: Requsername || null, tenhienthi: ReqTenhienthi || null, timeConnect: new Date().getTime() }
+    SocketPlayer[socket.id] = { username: Requsername || null, tenhienthi: ReqTenhienthi || null, timeConnect: new Date().getTime(), Ip: Ip }
     socket.on("disconnect", () => {
         //remove player
         delete SocketPlayer[socket.id]
