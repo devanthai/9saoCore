1299ea8da4c75a9e636b7558de1407396edf4637
devanthai <thaile2462@gmail.com>
2022-10-08 18:49:53 +0700

    dfgdfg

--- public/js/newSocket.js
+++ public/js/newSocket.js
@@ -3,8 +3,8 @@ if (document.domain.includes("localhost")) {
     socketNew = io.connect("http://" + document.domain + ":9956");
 }
 else {
-    socketNew = io.connect("https://socket.9sao.me", { query: "username="+usernameUser });
+    socketNew = io.connect("https://socket.9sao.me", { query: `username=${usernameUser}&tenhienthi=${tenhienThiUser}` });
 }
-socketNew.on("messageFromServer",(data)=>{
+socketNew.on("messageFromServer", (data) => {
     alert(data)
 })
